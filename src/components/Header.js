0f650d957ba246907523f030de49cf513869fa2c
Prince Thind <Princethind799@gmail.com>
2022-02-05 02:03:12 +0530

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="heading">Scarne's Dice</h1>
+      <div>
+        <div className="score player1-score">0</div>
+        <div className="score player2-score">0</div>
+      </div>
+    </header>
+  );
+}
