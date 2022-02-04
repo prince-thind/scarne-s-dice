3159d992cf865a730a84108baee01c92c46be6c8
Prince Thind <Princethind799@gmail.com>
2022-02-05 02:12:32 +0530

    integrate all components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import Dice from "./components/Dice";
+import Header from "./components/Header";
+import Pass from "./components/Pass";
 
 function App() {
+  const [turn, setTurn] = useState("player1");
+  const [scores, setScores] = useState({
+    player1: 0,
+    player2: 0,
+  });
+
   return (
     <div className="App">
-     
+      <Header scores={scores} turn={turn} />
+      <main>
+        <Dice setScores={setScores} turn={turn} setTurn={setTurn} />
+        <Pass setTurn={setTurn} />
+      </main>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-export default function Header() {
+export default function Header({scores,turn}) {
   return (
     <header>
       <h1 className="heading">Scarne's Dice</h1>
       <div>
-        <div className="score player1-score">0</div>
-        <div className="score player2-score">0</div>
+        <div className="score player1-score">{scores.player1}</div>
+        <div>{turn}'s turn</div>
+        <div className="score player2-score">{scores.player2}</div>
       </div>
     </header>
   );
